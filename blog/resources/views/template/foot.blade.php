d5b8763af4e7fd4b4b5c426d3db81e4581103b74
lgarin211 <lagustinus211@gmail.com>
2020-08-25 10:13:34 +0700

    laptop

--- blog/resources/views/template/foot.blade.php
+++ blog/resources/views/template/foot.blade.php
@@ -53,7 +53,14 @@
     <script src="{{ asset('data') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{ asset('data') }}/js/sb-admin-2.min.js"></script>
+
+ <!-- Page level plugins -->
+ <script src="{{ asset('data') }}/vendor/datatables/jquery.dataTables.min.js"></script>
+ <script src="{{ asset('data') }}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{ asset('data') }}/js/demo/datatables-demo.js"></script>
 
   </body>
 
