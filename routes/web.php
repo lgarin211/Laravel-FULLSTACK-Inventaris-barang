510158e169364b9bbeefff4f4cd1f53d8132d804
lgarin211 <lagustinus211@gmail.com>
2021-05-06 13:19:09 +0700

    las

--- routes/web.php
+++ routes/web.php
@@ -17,41 +17,43 @@ use PhpParser\Node\Stmt\Return_;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',function()
-{
-if(!empty(Auth::user()->id))
-	{[HomeController::class, 'dataread'];}
-else{return view('wek');}
-}
-Route::get('/try', function ()
-{
+
+Route::get('/', function () {
+    if (!empty(Auth::user()->id)) {
+        [HomeController::class, 'dataread'];
+    } else {
+        return view('wek');
+    }
+});
+
+Route::get('/try', function () {
     return view('wek');
 });
 
-Route::get('/insert',[read::class,'index']);
-Route::post('/insert','creat@creat');
-Route::get('/read_read',[read::class,'read']);
+Route::get('/insert', [read::class, 'index']);
+Route::post('/insert', 'creat@creat');
+Route::get('/read_read', [read::class, 'read']);
 Route::put('/ui/{id}', 'update@update');
 Route::put('/keluar/{id}', 'creat@cread_out');
 
-Route::get('/out',[read::class,'out_view']);
-Route::post('/out_find',[read::class,'data_out']);
+Route::get('/out', [read::class, 'out_view']);
+Route::post('/out_find', [read::class, 'data_out']);
 
-Route::get('/kategori_t/{view}/{table}',[read::class,'kategory']);
-Route::get('/kondisi_t/{view}/{table}',[read::class,'kategory']);
-Route::get('/lokasi_t/{view}/{table}',[read::class,'kategory']);
-Route::get('/key_primary/{view}/{table}',[read::class,'kategory']);
-Route::get('/komponen_t/{view}/{table}',[read::class,'kategory']);
-Route::post('/make_C/{table}','creat@creat_key');
+Route::get('/kategori_t/{view}/{table}', [read::class, 'kategory']);
+Route::get('/kondisi_t/{view}/{table}', [read::class, 'kategory']);
+Route::get('/lokasi_t/{view}/{table}', [read::class, 'kategory']);
+Route::get('/key_primary/{view}/{table}', [read::class, 'kategory']);
+Route::get('/komponen_t/{view}/{table}', [read::class, 'kategory']);
+Route::post('/make_C/{table}', 'creat@creat_key');
 
 Route::get('/pijam', function () {
-return view('find_pinjam');
+    return view('find_pinjam');
 });
 
-Route::put('/pin_in', [read::class,'b']);
-Route::put('/pin_out', [read::class,'a']);
-Route::put('send_news/{id}/{pic}','update@update_out');
-Route::put('send_news2/{id}/{pic}','update@update_out');
+Route::put('/pin_in', [read::class, 'b']);
+Route::put('/pin_out', [read::class, 'a']);
+Route::put('send_news/{id}/{pic}', 'update@update_out');
+Route::put('send_news2/{id}/{pic}', 'update@update_out');
 
 Auth::routes();
 
@@ -69,9 +71,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 // });
 
 
-        //Komputer
-Route::get('/komputer', [read::class,'komputer']);
+//Komputer
+Route::get('/komputer', [read::class, 'komputer']);
 Route::get('/buatpc', function () {
-return view('komputer.make');
+    return view('komputer.make');
 });
 Route::post('/kirimpc', 'creat@kirimpc');
